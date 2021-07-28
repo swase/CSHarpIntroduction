0f7b5da862f8cb769ebf2d9cb96f53847b155e8b
swase <fg2g15@soton.ac.uk>
2021-07-28 12:01:08 +0100

    Completed the Method.cs functions

--- 02b_TestFirstExercises/TestFirstExercises_Lib/Methods.cs
+++ 02b_TestFirstExercises/TestFirstExercises_Lib/Methods.cs
@@ -8,7 +8,7 @@ namespace TestFirstExercises
         // implement this method so that it returns true if x is greater than or equal to y
         public static bool GreaterEqual(int x, int y)
         {
-            return false;
+            return x >= y;
         }
 
         // Implement this method so that it take a double as an input,
@@ -16,13 +16,13 @@ namespace TestFirstExercises
         // Return a double rounded to 3 decimal places.
         public static double BODMAS(int inputNumber)
         {
-            return 0;
+            return Math.Round((((double)inputNumber * inputNumber) + 101 ) / 7 - 4, 3);
         }
 
         // implement this method so that it returns true if num is even, otherwise false
         public static bool EvenOdd(int num)
         {
-            return false;
+            return (num % 2) == 0;
         }
 
         // implement this method so that it returns 
@@ -30,21 +30,34 @@ namespace TestFirstExercises
         // that are divisible by either 2 or 5
         public static int SumEvenFive(int max)
         {
-            return -1;
+            int sum = 0;
+            for (int i = 1; i <= max; i++)
+            {
+                if (i % 2 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
         }
 
         // implement this method so it returns true if input is "password"
         // regardless of case
         public static bool CheckInput(string input)
         {
-            return false;
+            return input.ToLower().Equals("password");
         }
 
         // implement this method so it returns the sum
         // of all the numbers in the list
         public static int SumList(List<int> list)
         {
-            return -1;
+            int sum = 0;
+            foreach(int num in list)
+            {
+                sum += num;
+            }
+            return sum;
         }
     }
 }
--- 02b_TestFirstExercises/TestFirstExercises_Tests/01_OperatorTests.cs
+++ 02b_TestFirstExercises/TestFirstExercises_Tests/01_OperatorTests.cs
@@ -8,6 +8,9 @@ namespace TestFirstExercises_Tests
         [TestCase(5, 2, true)]
         [TestCase(2, 5, false)]
         [TestCase(5, 5, true)]
+        [TestCase(100,1000, false)]
+        [TestCase(12354, 12352, true)]
+        [TestCase(100, 100, true)]
         public void GivenTwoNumbersXY_GreaterEqual_ReturnsWhetherXIsGreatorOrEqualToY(int x, int y, bool expResult)
         {
             Assert.That(Methods.GreaterEqual(x, y), Is.EqualTo(expResult));
