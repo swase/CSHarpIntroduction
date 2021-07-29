676735797bd1569364da2d3302de83a10469a87c
swase <fg2g15@soton.ac.uk>
2021-07-30 00:16:18 +0100

    Completed operators and control flow lab. Added extra exception handling to methods.

--- Operators_ControlFlow_Lab_Starter/Op_CtrlFlow_Lib/Exercises.cs
+++ Operators_ControlFlow_Lab_Starter/Op_CtrlFlow_Lib/Exercises.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
+using System;
 
 namespace Op_CtrlFlow
 {
     public class Exercises
     {
+        //Returns false if numbers are the same, else will return true if num2 is a multple of num1
         public static bool MyMethod(int num1, int num2)
         {
             return num1 == num2 ? false : (num1 % num2) == 0;
@@ -12,7 +14,17 @@ namespace Op_CtrlFlow
         // returns the average of the array nums
         public static double Average(List<int> nums)
         {
-            return -1;
+            if (nums.Count == 0)
+            {
+                throw new NullReferenceException("List passed cannot be null");
+            }
+            double sum = 0;
+            foreach(int num in nums)
+            {
+                sum += num;
+            }
+
+            return sum / nums.Count;
         }
 
         // returns the type of ticket a customer is eligible for based on their age
@@ -24,18 +36,104 @@ namespace Op_CtrlFlow
         public static string TicketType(int age)
         {
             string ticketType = string.Empty;
+            if (age < 0)
+            {
+                throw new ArgumentOutOfRangeException("Cannot have an age < 0");
+            }
+            else if (age < 5)
+            {
+                ticketType = "Free";
+            }
+            else if (age >= 5 && age <= 12)
+            {
+                ticketType = "Child";
+            }
+
+            else if (age > 12 && age < 18)
+            {
+                ticketType = "Student";
+            }
+
+            else if (age >= 18 && age < 60)
+            {
+                ticketType = "Standard";
+            }
+
+            else
+            {
+                ticketType = "OAP";
+            }
+            
             return ticketType;
         }
 
         public static string Grade(int mark)
         {
-            var grade = "";
-            return grade;
+            string outcome = "";
+            if (mark < 0 || mark > 100)
+            {
+                throw new ArgumentOutOfRangeException("Mark must be >= 0 and <= 100");
+            }
+            else
+            {
+                return mark >= 40 ? "Pass" + (mark >= 60 ?  " with " + (mark >= 75 ? "Distinction" : "Merit") : "") : "Fail";
+            }
+
+            //else
+            //{
+            //    if (mark > 40)
+            //    {
+            //        outcome += "Pass";
+            //        if (mark >= 60)
+            //        {
+            //            outcome += " with ";
+            //            if (mark >= 75)
+            //            {
+            //                outcome += "Distinction";
+            //            }
+            //            else
+            //            {
+            //                outcome += "Merit";
+            //            }
+            //        }
+            //    }
+
+            //    else { outcome += "Fail"; }
+            //}
+
+            //return outcome;
+
         }
 
         public static int GetScottishMaxWeddingNumbers(int covidLevel)
         {
-            return 0;
+            if (covidLevel < 0 || covidLevel > 4)
+            {
+                throw new ArgumentOutOfRangeException("Covide levels must be between 0 and 4 inclusive");
+            }
+            int maxCapacity = 0;
+            switch(covidLevel)
+            {
+                case 0:
+                    maxCapacity = 200;
+                    break;
+                
+                case 1:
+                    maxCapacity = 100;
+                    break;
+                
+                case 2:
+                
+                case 3:
+                    maxCapacity = 50;
+                    break;
+                
+                case 4:
+                    maxCapacity = 20;
+                    break;
+
+            }
+            return maxCapacity;
         }
     }
 }
--- Operators_ControlFlow_Lab_Starter/Op_CtrlFlow_Tests/Exercises_Tests.cs
+++ Operators_ControlFlow_Lab_Starter/Op_CtrlFlow_Tests/Exercises_Tests.cs
@@ -1,13 +1,24 @@
 ﻿using NUnit.Framework;
 using Op_CtrlFlow;
 using System.Collections.Generic;
+using System;
 
 namespace Op_CtrlFlow_Tests
 {
     public class Exercises_Tests
     {     
         // write unit test(s) for MyMethod here
-
+        [TestCase(1,1, false)]
+        [TestCase(0, 0, false)]
+        [TestCase(24, 24, false)]
+        [TestCase(27, 52, false)]
+        [TestCase(27, 34, false)]
+        [TestCase(20, 60, false)]
+        [TestCase(20, 50, false)]
+        public void MyMethodTest_returnsFalse(int num1, int num2, bool expectedValue)
+        {
+            Assert.That(Exercises.MyMethod(num1, num2), Is.EqualTo(expectedValue));
+        }
 
         [Test]
         public void Average_ReturnsCorrectAverage()
@@ -20,8 +31,10 @@ namespace Op_CtrlFlow_Tests
         public void WhenListIsEmpty_Average_ReturnsZero()
         {
             var myList = new List<int>() {};
-            Assert.That(Exercises.Average(myList), Is.EqualTo(0));
+            Assert.That(() => Exercises.Average(myList),
+                Throws.TypeOf<NullReferenceException>().With.Message.Contains("cannot be null"));
         }
+
 
         [TestCase(100, "OAP")]
         [TestCase(60, "OAP")]
@@ -38,5 +51,75 @@ namespace Op_CtrlFlow_Tests
             var result = Exercises.TicketType(age);
             Assert.That(result, Is.EqualTo(expected));
         }
+
+        [TestCase(-25)]
+        [TestCase(-1)]
+        [TestCase(101)]
+        [TestCase(200)]
+        public void  GradeTest_givenAGradeLessThan0OrGreaterThan100_ReturnsArgumentOutOfRange(int grade)
+        {
+            Assert.That(() => Exercises.Grade(grade),
+                Throws.TypeOf<ArgumentOutOfRangeException>().With.Message.Contains("Mark must be >= 0 and <= 100"));
+        }
+
+        [TestCase(75, "Pass with Distinction")]
+        [TestCase(76, "Pass with Distinction")]
+        [TestCase(90, "Pass with Distinction")]
+        [TestCase(99, "Pass with Distinction")]
+        [TestCase(100, "Pass with Distinction")]
+        public void GradeTest_givenAGradeofGreaterThan75_ReturnsDistinciton(int grade, string expectedValue)
+        {
+            Assert.That(Exercises.Grade(grade), Is.EqualTo(expectedValue));
+        }
+
+        [TestCase(60, "Pass with Merit")]
+        [TestCase(61, "Pass with Merit")]
+        [TestCase(70, "Pass with Merit")]
+        [TestCase(74, "Pass with Merit")]
+        public void GradeTest_givenAGradeofGreater59LessThan74_ReturnsMerit(int grade, string expectedValue)
+        {
+            Assert.That(Exercises.Grade(grade), Is.EqualTo(expectedValue));
+        }
+
+        [TestCase(40, "Pass")]
+        [TestCase(41, "Pass")]
+        [TestCase(55, "Pass")]
+        [TestCase(59, "Pass")]
+        public void GradeTest_givenAGradeofGreater40LessThan60_ReturnsPass(int grade, string expectedValue)
+        {
+            Assert.That(Exercises.Grade(grade), Is.EqualTo(expectedValue));
+        }
+
+        [TestCase(0, "Fail")]
+        [TestCase(1, "Fail")]
+        [TestCase(25, "Fail")]
+        [TestCase(39, "Fail")]
+        public void GradeTest_givenAGradeofLessThan40_ReturnsFail(int grade, string expectedValue)
+        {
+            Assert.That(Exercises.Grade(grade), Is.EqualTo(expectedValue));
+        }
+
+        [TestCase(-10)]
+        [TestCase(-1)]
+        [TestCase(5)]
+        [TestCase(10)]
+        public void GetScottishMaxWeddingNumbersTest_GivenLevelLessThan0OrGreaterThan4_ReturnsArugmentOutOfRange(int level)
+        {
+            Assert.That(() => Exercises.GetScottishMaxWeddingNumbers(level),
+                Throws.TypeOf<ArgumentOutOfRangeException>().With.Message.Contains("must be between 0 and 4 inclusive"));
+        }
+
+        [TestCase(0, 200)]
+        [TestCase(1, 100)]
+        [TestCase(2, 50)]
+        [TestCase(3, 50)]
+        [TestCase(4, 20)]
+
+        public void GetScottishMaxWeddingNumbersTest_GivenLevel_ReturnsExpectedValue(int level, int expectedValue)
+        {
+            Assert.That(Exercises.GetScottishMaxWeddingNumbers(level),
+                Is.EqualTo(expectedValue));
+        }
+
     }
 }
